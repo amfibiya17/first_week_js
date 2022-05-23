798613bebce2e4091f83c27d0fabc7430ddafb31
Yaroslava Yates <yaroslava.yates@gmail.com>
2022-05-23 12:44:55 +0100

    02_running_files

---
+++ README.md
@@ -0,0 +1,4 @@
+%s format a variable as a string
+%d format a variable as a number
+%i format a variable as its integer part only
+%o format a variable as an object
--- first_day.js
+++ first_day.js
@@ -1,2 +1,15 @@
-const name = `Slava`;
-console.log(`Hello, ` + name + `!`);
+// 01_running_javascript
+const myName = `Slava`;
+console.log(`Hello, ` + myName + `!`);
+
+// 02_running_files
+console.log(`Hello - I am a file!`);
+
+// Write, in a file hello.js, a JavaScript program that logs a greeting message to the console
+// Run this program. You should get the greeting message logged in your terminal
+console.log(`Hello, there!`);
+
+// Run a JavaScript file that logs the result of 2 + 2 in the console
+const a = 2;
+const b = 2;
+console.log(2 + 2);
