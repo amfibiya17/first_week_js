21fe2d1d59d2c8e3c8a3c7d32d26ed0eb7a0d995
Yaroslava Yates <yaroslava.yates@gmail.com>
2022-05-24 22:35:20 +0100

    02_search_function

--- spec/02_searchCandies.test.js
+++ spec/02_searchCandies.test.js
@@ -1,2 +1,25 @@
-// const searchCandies = require('../lib/phase_2/02_searchCandies.js');
+const searchCandies = require('../lib/phase_2/02_searchCandies.js');
 
+// Use the toEqual() Jest matcher
+
+describe('searchCandies', () => {
+  it('returns Aero if max price is 2 and search name is Aero', () => {
+    expect(searchCandies('Aero', 2)).toEqual(['Aero']);
+  });
+ 
+  it('returns Skitties if max price is 4 and search name is Skitties', () => {
+    expect(searchCandies('Skitties', 4)).toEqual(['Skitties']);
+  });
+
+  it('returns Mars, Maltesers if max price is 10 and search name is Ma', () => {
+    expect(searchCandies('Ma', 10)).toEqual(['Mars', 'Maltesers']);
+  });
+
+  it('returns Skitties, Skittles, Starburst if max price is 10 and search name is S', () => {
+    expect(searchCandies('S', 10)).toEqual(['Skitties', 'Skittles', 'Starburst']);
+  });
+
+  it('returns Skitties, Skittles if max price is 4 and search name is S', () => {
+    expect(searchCandies('S', 4)).toEqual(['Skitties', 'Skittles']);
+  });
+});
