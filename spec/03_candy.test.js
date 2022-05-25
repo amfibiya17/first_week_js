332fc4f1caf0d08dcd13a419fe26a4a568714ccb
Yaroslava Yates <yaroslava.yates@gmail.com>
2022-05-25 12:13:47 +0100

    03_shopping_basket

--- spec/03_candy.test.js
+++ spec/03_candy.test.js
@@ -1,7 +1,19 @@
 const Candy = require('../lib/phase_2/03_candy.js');
 
-describe('', () => {
-  it('', () => {
+describe('Candy', () => {
 
+  let candy = new Candy('Aero', 1.99)
+
+  describe('getName', () => {
+    it('returns the name of the candy', () => {
+      expect(candy.getName()).toBe('Aero')
+    });
   });
+
+  describe('getPrice', () => {
+    it('returns the price of the candy', () => {
+      expect(candy.getPrice()).toBe(1.99)
+    });
+  });
+
 });
--- spec/03_shoppingBasket.test.js
+++ spec/03_shoppingBasket.test.js
@@ -1,7 +1,41 @@
 const ShoppingBasket = require('../lib/phase_2/03_shoppingBasket.js');
 
-describe('', () => {
-  it('', () => {
+describe('ShoppingBasket', () => {
+  
+  let doubleAero = { getName: 'Aero', getPrice: 1.99 };
 
+  describe('getTotalPrice', () => {
+    it('returns 0 when there is nothing in products basket', () => {
+      let basket = new ShoppingBasket();
+
+      expect(basket.getTotalPrice()).toBe(0)
+    });
+
+    it('returns the total price of the products basket', () => {
+      let basket = new ShoppingBasket();
+      basket.addItem(doubleAero);
+
+      expect(basket.getTotalPrice()).toBe(1.99);
+    });
   });
+
+  describe('addItem', () => {
+    it('can add a chosen item into the products basket', () => {
+      let basket = new ShoppingBasket();
+      basket.addItem(doubleAero);
+      
+      expect(basket.products[0]).toBe(doubleAero)
+    });
+  });
+
+  describe('applyDiscount', () => {
+    it('can deduce the products basket price when discount applied', () => {
+      let basket = new ShoppingBasket();
+      basket.addItem(doubleAero);
+      basket.applyDiscount(1);
+
+      expect(basket.getTotalPrice()).toBe(0.99)
+    });
+  });
+
 });
