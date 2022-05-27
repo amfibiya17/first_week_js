66bb7308f01e40ad20536e055733cdd3879243ab
Yaroslava Yates <yaroslava.yates@gmail.com>
2022-05-27 10:31:06 +0100

    counter

--- phase_2/07_web_server/server.js
+++ phase_2/07_web_server/server.js
@@ -1,1 +1,26 @@
 const express = require('express');
+const app = express();
+const port = 3000;
+
+let counter = 0;
+
+app.get('/', (req, res) => {
+  res.send('Hello, express! My name is Slava!')
+});
+
+app.post('/counter', (req, res) => {
+  counter += 1;
+  res.send(`${counter}`)
+});
+
+app.get('/counter', (req, res) => {
+  res.send(`${counter}`)
+});
+
+app.delete('/counter', (req, res) => {
+  counter = 0;
+  res.send(`${counter}`)
+});
+
+console.log(`Server listening on localhost:${port}`);
+app.listen(port);
